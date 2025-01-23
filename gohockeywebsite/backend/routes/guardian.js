849029bfe37d7bdfe9127616473e8630aa3bc620
Noah Kostesku <noahkwoz29@icloud.com>
2025-01-23 11:27:01 -0500

    Updated schema

---
+++ gohockeywebsite/backend/routes/guardian.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+const { /*fill in with functions from controller file */} = require('../controllers/guardianController');
+
+//list CRUD ops
+
+module.exports = router;
---
+++ gohockeywebsite/backend/routes/payment.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { /*fill in with functions from controller file */} = require('../controllers/paymentController');
+
+//list CRUD ops
+
+module.exports = router;
